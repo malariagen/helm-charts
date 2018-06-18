afdf7e8291edfd4816b55d5ac0b35dc612276388
Slejdops <slejdops@gmail.com>
2018-06-18 11:44:04 +0100

    Added the malariagen chart

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+helm init --client-only
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
+helm dependency update malariagen
+
+#GIT_SSH_COMMAND="ssh -i ${PWD}/helm-chart-deploy-key" chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --publish-chart
+
