94b9f13a75ae4c928927c26126701457f783928f
Slejdops <slejdops@gmail.com>
2018-06-20 10:40:14 +0100

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+openssl aes-256-cbc -K $encrypted_ed741c330351_key -iv $encrypted_ed741c330351_iv -in mgen_github_deploy_key.enc -out mgen_github_deploy_key -d
+chmod 0400 mgen_github_deploy_key
 
 helm init --client-only
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
@@ -7,5 +8,5 @@ helm repo update
 
 helm dependency update malariagen
 
-#GIT_SSH_COMMAND="ssh -i ${PWD}/helm-chart-deploy-key" chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --publish-chart
+GIT_SSH_COMMAND="ssh -i ${PWD}/mgen_github_deploy_key" chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --publish-chart
 
